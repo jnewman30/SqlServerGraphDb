809f8fed913526653a128935863bb7be67c25a67
Jeremy Newman <jnewman30@gmail.com>
2018-06-25 13:16:18 -0500

    Fix Angular Dependencies

--- GraphDbWeb/src/app/shared/modules/layout/layout.module.ts
+++ GraphDbWeb/src/app/shared/modules/layout/layout.module.ts
@@ -3,21 +3,61 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 
 import {
-    MatCommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule
+    MatCommonModule, MatButtonModule, MatListModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule, MatIconModule,
+    MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
+    MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
+    MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule
 } from '@angular/material';
+
+import {
+    CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule,
+    CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
+    CovalentNotificationsModule, CovalentMenuModule, CovalentDataTableModule, CovalentMessageModule
+} from '@covalent/core';
 
 @NgModule({
     imports: [
         CommonModule,
 
-        /* Angular Material */
+        /** Material Modules */
         MatCommonModule,
-        MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatListModule,
+        MatIconModule,
+        MatCardModule,
+        MatMenuModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonToggleModule,
+        MatSlideToggleModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        MatToolbarModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatTooltipModule,
+        MatRippleModule,
+        MatRadioModule,
+        MatGridListModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSliderModule,
+        MatAutocompleteModule,
+        /** Covalent Modules */
+        CovalentCommonModule,
+        CovalentLayoutModule,
+        CovalentMediaModule,
+        CovalentExpansionPanelModule,
+        CovalentStepsModule,
+        CovalentDialogsModule,
+        CovalentLoadingModule,
+        CovalentSearchModule,
+        CovalentPagingModule,
+        CovalentNotificationsModule,
+        CovalentMenuModule,
+        CovalentDataTableModule,
+        CovalentMessageModule
     ],
     declarations: [LayoutComponent],
     exports: [LayoutComponent]
